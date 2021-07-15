304c5cf8e2d8d1cbe3832980a2089941f79c68dd
timor66 <timo.johan.rinne@gmail.com>
2021-07-15 09:28:44 +0300

    4 files modified, 1 added

---
+++ euroopan-junaraiteet.js
@@ -0,0 +1,50 @@
+import { LitElement, html } from 'lit-element';
+import mapboxgl from 'mapbox-gl';
+
+const MAB_BOX_TIMEOUT = 60000000;
+const MAPBOX_TOKEN =
+  'pk.eyJ1IjoidGltb3I2NiIsImEiOiJja3F3ODczd3UwNTJ4MndueHBkdjB5c3dsIn0.68mu1Rk-3ZMPqlzBF_HknQ';
+
+class MapBoxComponent extends LitElement {
+  constructor() {
+    super();
+  }
+
+  firstUpdated() {
+    this.initMap();
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+
+  initMap() {
+    let options = {
+      enableHighAccuracy: true,
+      timeout: MAB_BOX_TIMEOUT,
+      maximumAge: 0
+    };
+    this.buildMap();
+  }
+
+  buildMap() {
+    mapboxgl.accessToken = MAPBOX_TOKEN;
+  
+    this.map = new mapboxgl.Map({
+      container: 'railroads-map',
+      style: 'mapbox://styles/timor66/ckr32cq3pemln18qspaqpkqiq',
+      center: [14.5, 53.5],
+      zoom: 4
+    });
+  }
+   
+  render() {
+    return html`
+      <div id="railroads-map"></div>
+    `;
+  }
+} 
+
+//Component registration
+customElements.define('mapbox-component', MapBoxComponent);
+    
